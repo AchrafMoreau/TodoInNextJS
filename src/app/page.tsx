13e86b2751f25a1356a5c1ae42f8329fa419d481
AchrafMoreau <elachraf6@gmail.com>
2024-10-01 17:01:19 +0100

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,32 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+const mockUrls = [
+  "https://utfs.io/f/pj0T1BSISEVBkXBIMR5X5V8RtaZrjl9YIocfGAv3gzuUBE7K",
+  "https://utfs.io/f/pj0T1BSISEVBFyYbGtOfTIs1GMqJ9AiyPv8arNhdQ7mkC3YH",
+  "https://utfs.io/f/pj0T1BSISEVB0AKka3tJh7lLA8DnyMOWZxu1zbRitEdpme3T",
+  "https://utfs.io/f/pj0T1BSISEVBnRzMgl0G5ITbL47Fdu63A0x18zw9mYSZpQEX",
+]
 
-  const mockUrls = [
-    "https://utfs.io/f/pj0T1BSISEVBkXBIMR5X5V8RtaZrjl9YIocfGAv3gzuUBE7K",
-    "https://utfs.io/f/pj0T1BSISEVBFyYbGtOfTIs1GMqJ9AiyPv8arNhdQ7mkC3YH",
-    "https://utfs.io/f/pj0T1BSISEVB0AKka3tJh7lLA8DnyMOWZxu1zbRitEdpme3T",
-    "https://utfs.io/f/pj0T1BSISEVBnRzMgl0G5ITbL47Fdu63A0x18zw9mYSZpQEX",
-  ]
+const mockImages = mockUrls.map((elm, index) => ({
+  id: index +1,
+  url: elm
+}))
+export default async function HomePage() {
 
-  const mockImages = mockUrls.map((elm, index) => ({
-    id: index +1,
-    url: elm
-  }))
+  const posts = await db.query.posts.findMany();
+  console.log(posts)
+
   return (
     <main className="">
         <div className="flex flex-wrap gap-4">
+          {posts.map((post)=>(
+            <div className="elm" key={post.id}>
+              {post.name}
+            </div>
+          ))}
           {mockImages.map((image)=> (
-            <div key={image.id} className="w-48">
+            <div key={image.id + 1} className="w-48">
               <img src={image.url} alt="image" />
             </div>
           ))}
